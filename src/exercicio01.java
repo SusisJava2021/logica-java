d5e5dbd0fc00de4b1124012675e4ac6779d2817d
Entra21 <entra21@proway.treina>
2021-05-11 20:24:08 -0300

    exercicio01

--- src/exercicio01.java
+++ src/exercicio01.java
@@ -19,6 +19,16 @@ public class exercicio01 {
         double price2 = 650.50;
         double measure = 53.234567;
 
+        System.out.println("Products");
+        System.out.printf("%s, Wich price is $ %.2f%n" , product1, price1);
+        System.out.printf("%s, Wich price is $ %.2f%n" , product2, price2);
+        System.out.println();
+        System.out.printf("Record: %d years old, code %d anda gender: %c%n" , age , code , gender);
+        System.out.println();
+        System.out.printf("Measue with eight decimal places: %.8f%n" , measure);
+        System.out.printf("Rouded (three decimal places): %.3f%n" , measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f%n" , measure);
 
 
     }
