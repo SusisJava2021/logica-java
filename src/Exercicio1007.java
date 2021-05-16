cb324e698304b53982bfecf1afd26187f6319c24
Alencar <Susana.Alencar@br.experian.com>
2021-05-16 14:32:09 -0300

    Exercicio1007

---
+++ src/Exercicio1007.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercicio1007 {
+
+
+    public static void main(String)[] args) {
+         
+        Scanner scanner = new Scanner(System.in);
+
+        // *** Entrada de dados ***//
+
+        int A = 5;
+        int B = 6;
+        int C = 7;
+        int D = 8;
+
+         // *** PROCESSAMENTO ***
+
+        int diferenca = (A*B-C*D);
+
+         // ***SAIDA DE DADOS***
+
+         System.out.printf("DIFERENCA = %d/n", diferenca);
+
+
+
+
+    }
+
+}
---
+++ src/SequenciaEstrutural.java
@@ -0,0 +1,23 @@
+public class SequenciaEstrutural { 
+    
+    
+    public static void main(String[] args) {
+    // *** Entrada de dados ***//
+
+    // declaracando | criando uma variavel - do TIPO String
+    // do TIPO String(Texto alfanumerico) e TIPO int(inteiro sem virgula)
+    String nome, sobrenome, nomeCompleto;
+    int idade;
+
+    // setando o valor da variavel | atribuiu o valor a variavel
+    nome = "Susana";
+    sobrenome = "Alencar";
+    idade = 40;
+
+    // *** Processamento de dados ***//
+    frase - "Oi eu sou a " + nome + " " + sobrenome + "e tenho " + idade + "anos";
+   
+    // *** Saida de dados ***//
+    System.out.println(frase);
+    }
+    }
