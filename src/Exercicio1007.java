f2c338047542e3ee89f26fdd0fee46f1fecd4667
Alencar <Susana.Alencar@br.experian.com>
2021-05-16 16:12:17 -0300

    Perfumaria1007

--- src/Exercicio1007.java
+++ src/Exercicio1007.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 
 public class Exercicio1007 {
 
+    /* Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo
+     a fórmula: DIFERENCA = (A * B - C * D).
 
-    public static void main(String)[] args) {
+    Entrada - O arquivo de entrada contém 4 valores inteiros.
+
+    Saída - Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+
+    Exemplos de Entrada	            Exemplos de Saída
+            5                        DIFERENCA = -26
+            6
+            7
+            8
+
+        /*
+
+
+    public static void main(String[] args) {
          
         Scanner scanner = new Scanner(System.in);
 
@@ -18,9 +33,9 @@ public class Exercicio1007 {
 
         int diferenca = (A*B-C*D);
 
-         // ***SAIDA DE DADOS***
+         // ***Saida de dados***
 
-         System.out.printf("DIFERENCA = %d/n", diferenca);
+         System.out.println("DIFERENCA = %d/n", diferenca);
 
 
 
