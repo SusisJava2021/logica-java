240a77be4089cfbe6c5c10ad29d2f89cb5f94686
Alencar <Susana.Alencar@br.experian.com>
2021-05-17 12:01:08 -0300

    Exercicio1014

---
+++ src/Exercicio1014.java
@@ -0,0 +1,42 @@
+public class Exercicio1014{
+
+/** Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível
+ gasto (em litros).
+
+Entrada - O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km), 
+e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+Saída - Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l".
+
+Exemplo de Entrada              	Exemplo de Saída
+        500                           14.286 km/l
+        35.0
+
+
+/*
+
+              
+            
+            // *** Entrada de dados ***/
+
+            public static void main(String[] args){
+
+
+            Scanner scanner = new Scanner(System.in);
+            int x = scanner.nextInt();
+            double y = scanner.nextDouble();
+
+            // *** Processamento ***
+
+            double media = x / y;
+
+            // ***Saida de dados***
+
+            System.out.println(String.format("%.3f km/l", media));   
+        }
+        
+    }
+
+
+
+
